cb7e6627db5a95f530292636eacca9e6864da3dc
theanaoliveira <carolineana@gmail.com>
2018-04-19 23:37:46 -0300

    Inclusão de projeto

---
+++ ConsoleChatBot/ChatBotDialog.cs
@@ -0,0 +1,35 @@
+﻿using Syn.Bot.Oscova;
+using Syn.Bot.Oscova.Attributes;
+using System.Diagnostics;
+
+namespace ConsoleChatBot
+{
+    class ChatBotDialog : Dialog
+    {
+        [Expression("Oi")]
+        [Expression("Ola")]
+        [Expression("Hello")]
+        public void HelloBot(Result result)
+        {
+            var response = new Response
+            {
+                Text = "<b>Hello bot developer!</b>",
+                Format = ResponseFormat.Html
+            };
+
+            result.SendResponse(response);
+        }
+
+        [Expression("Tudo bem?")]
+        public void HelloBot2(Result result)
+        {
+            result.SendResponse("Tudo otimo, e com você?");
+        }
+
+        
+        public void HelloBotGeral(Result result)
+        {
+            result.SendResponse("Desculpe, não entendi");
+        }
+    }
+}
---
+++ ConsoleChatBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Syn.Bot.Oscova;
+using System;
+
+namespace ConsoleChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lblnSair = false;            
+
+            while (!lblnSair)
+            {
+                var bot = new OscovaBot();
+                bot.Dialogs.Add(new ChatBotDialog());
+                bot.Trainer.StartTraining();
+
+                bot.MainUser.ResponseReceived += (sender, eventArgs) =>
+                {
+                    Console.WriteLine($"Panelinha: {eventArgs.Response.Text}");
+                };
+
+                Console.Write("Usuário: ");
+                var message = Console.ReadLine();
+
+                var result = bot.Evaluate(message);
+                result.Invoke();
+
+                if (message.Equals("sair"))
+                    lblnSair = true;
+            }            
+        }
+    }
+}
